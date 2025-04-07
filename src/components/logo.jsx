a7323c6eac2a73467ed66b1319d7e13c1afe6ad5
jihee1103 <sweet9811@gmail.com>
2025-04-07 16:26:27 +0900

    파일 경로 변경

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,21 +1,19 @@
 export function ScienceLogo() {
-  return (
-    <img src="/src/assets/science-logo.svg" alt="과학기술정보통신부 로고" />
-  );
+  return <img src="/assets/science-logo.svg" alt="과학기술정보통신부 로고" />;
 }
 
 export function IndustryLogo() {
-  return <img src="/src/assets/industry-logo.svg" alt="산업통상자원부 로고" />;
+  return <img src="/assets/industry-logo.svg" alt="산업통상자원부 로고" />;
 }
 
 export function MinistryLogo() {
-  return <img src="/src/assets/ministry-logo.svg" alt="중소벤처기업부 로고" />;
+  return <img src="/assets/ministry-logo.svg" alt="중소벤처기업부 로고" />;
 }
 
 export function EtnewsLogo() {
   return (
     <img
-      src="/src/assets/etnews-logo.svg"
+      src="/assets/etnews-logo.svg"
       alt="전자신문 로고"
       style={{ position: "relative", top: "-5px" }}
     />
