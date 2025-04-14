377b7acd3eb2f45b71d6eee1a4932fa0e54b1b18
jihee1103 <sweet9811@gmail.com>
2025-04-14 14:51:58 +0900

    contact페이지 완성

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,33 @@
-import "./about.css";
+import styles from "./about.module.scss";
 
 function About() {
-  return <>About</>;
+  return (
+    <>
+      <div className={styles.pageWrapper}>
+        <div className={styles.contactContainer}>
+          <div className={styles.title}>CONTACT</div>
+          <div className={styles.underline}></div>
+
+          <div className={styles.infoWrapper}>
+            <div className={styles.infoBox}>
+              <div className={styles.companyName}>전자신문 어워드 사무국</div>
+
+              <div className={styles.contactInfo}>
+                <div className={styles.contactItem}>
+                  <span className={styles.contactLabel}>E-mail</span>
+                  <span>etnews-ad@naver.com</span>
+                </div>
+                <div className={styles.contactItem}>
+                  <span className={styles.contactLabel}>Tel</span>
+                  <span>02-2201-9502</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
