db93ec66decb4e3a7261e92af83c2884224b4c81
jihee1103 <sweet9811@gmail.com>
2025-10-14 13:06:44 +0900

    일정 변경

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -4,6 +4,7 @@ import styles from "./schedule.module.scss";
 function Schedule({ type }) {
   // type 파라미터를 사용하여 조건부 렌더링
   if (type === "digital") {
+    //k-digital대상
     return (
       <div>
         <IndexTitle type={"digital"}>일정</IndexTitle>
@@ -21,24 +22,26 @@ function Schedule({ type }) {
       </div>
     );
   } else if (type === "mobile") {
+    //모바일서비스 대상
     return (
       <div>
         <IndexTitle type={"mobile"}>일정</IndexTitle>
         <ul className={styles.schedule}>
           <li>접수기간</li>
           <p>
-            2025년 9월 17일 ~ 2025년 10월 23일
+            2025년 9월 17일 ~ 2025년 11월 14일
             <br />※ 부문별 마감 기한 상이
           </p>
           <li>전문가 평가 및 최종 심의</li>
-          <p>2025년 10월 24일</p>
+          <p>2025년 10월 24일, 11월 14일</p>
           <li>선정 발표</li>
-          <p>2025년 11월 20일 (목) (예정)</p>
+          <p>2025년 12월 1일 (월)</p>
         </ul>
       </div>
     );
   } else if (type === "appliance") {
     return (
+      //가전대상
       <div>
         <IndexTitle type={"appliance"}>일정</IndexTitle>
         <ul className={styles.schedule}>
