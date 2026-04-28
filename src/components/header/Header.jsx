d4ec2d9372c0bac8bd412333c9e40fdc5f914ec6
jihee1103 <sweet9811@gmail.com>
2026-04-28 15:13:51 +0900

    디지털대상 일정 변경

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,13 +18,13 @@ function Header() {
         <nav className="category">
           <ul>
             <li>
+              <Link to="/k-digital">K-디지털브랜드 대상</Link>
+            </li>
+            <li>
               <Link to="/appliance">대한민국 가전대상</Link>
             </li>
             <li>
               <Link to="/mobile">대한민국 모바일서비스 대상</Link>
-            </li>
-            <li>
-              <Link to="/k-digital">K-디지털브랜드 대상</Link>
             </li>
             <li>
               <Link to="/about">CONTACT</Link>
